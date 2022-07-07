abe3c3e85fdde8dfeb18f473d2c4f8d869de5e92
jelidahjay <nayingwaj@gmail.com>
2022-07-07 22:10:40 +1400

    Added common route constants and message constants.

---
+++ ExpenseTracker.Utilities/Constants/MessageConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace ExpenseTracker.Utilities.Constants
+{
+    /// <summary>
+    /// Entity containing common constants commonly used by other entities.
+    /// </summary>
+    public static class MessageConstants
+    {
+        /// <summary>
+        /// Message to display that a field is required.
+        /// </summary>
+        public const string RequiredError = "Required!";
+
+        /// <summary>
+        /// Error message to show thhe maximum length of field has been reached.
+        /// </summary>
+        public const string LengthError = "Maximum length reached!";
+    }
+}
---
+++ ExpenseTracker.Utilities/Constants/RouteConstants.cs
@@ -0,0 +1,63 @@
+﻿namespace ExpenseTracker.Utilities.Constants
+{
+    /// <summary>
+    /// Entity containing all the common routes used in the controllers.
+    /// </summary>
+    public static class RouteConstants
+    {
+        /// <summary>
+        /// Route : Categories controller.
+        /// </summary>
+        public const string ExpenseCategoriesController = "api/expense-tracker";
+
+        /// <summary>
+        /// Route : Get all categories.
+        /// </summary>
+        public const string Categories = "categories";
+
+        /// <summary>
+        /// Route : Get category by ID.
+        /// </summary>
+        public const string CategoryByKey = "category/key/";
+
+        /// <summary>
+        /// Route : Post - create a new category.
+        /// </summary>
+        public const string CreateCategory = "category/create";
+
+        /// <summary>
+        /// Route : Put - update a category.
+        /// </summary>
+        public const string UpdateCategory = "category/update/key/";
+
+        /// <summary>
+        /// Route : Delete - delete a category.
+        /// </summary>
+        public const string DeleteCategory = "category/delete/key/";
+
+        /// <summary>
+        /// Route : Get all expenses.
+        /// </summary>
+        public const string Expenses = "expenses";
+
+        /// <summary>
+        /// Route : Get expenses by ID.
+        /// </summary>
+        public const string ExpenseByKey = "expense/key/";
+
+        /// <summary>
+        /// Route : Post - create a new expense.
+        /// </summary>
+        public const string CreateExpense = "expense/create";
+
+        /// <summary>
+        /// Route : Put - update an expense.
+        /// </summary>
+        public const string UpdateExpense = "expense/update/key/";
+
+        /// <summary>
+        /// Route : Delete - delete an expense.
+        /// </summary>
+        public const string DeleteExpense = "expense/delete/key/";
+    }
+}
