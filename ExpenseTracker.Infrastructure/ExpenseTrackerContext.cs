bab570487d025037462781dc6e84f74ac3b5e762
jelidahjay <nayingwaj@gmail.com>
2022-07-07 22:09:24 +1400

    Added data context and migrations for entities

---
+++ ExpenseTracker.Infrastructure/ExpenseTrackerContext.cs
@@ -0,0 +1,31 @@
+﻿using ExpenseTracker.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExpenseTracker.Infrastructure
+{/// <summary>
+/// Data Context Class that interacts with the database.
+/// </summary>
+    public class ExpenseTrackerContext : DbContext
+    {
+        /// <summary>
+        /// DbSet to represent entities from the Category model.
+        /// </summary>
+        public DbSet<Category> Categories { get; set; }
+
+        /// <summary>
+        /// DbSet to represent entities from the Expense model.
+        /// </summary>
+        public DbSet<Expense> Expenses { get; set; }
+
+        /// <summary>
+        /// Create a connection string to connect SQL server.
+        /// </summary>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=.\\SQLEXPRESS;Initial " +
+                                      "Catalog=ExpenseTracker;" +
+                                      "User Id=jelida;Password=Jelidah1998#;trusted_connection = true";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
